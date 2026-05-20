891b98e0fd2d2728e2103728cc5f7dec7a30f10f
josemuanza069-ship-it <josemuanza069@gmail.com>
2026-05-20 12:15:19 +0100

    Add files via upload

---
+++ efeito.js
@@ -0,0 +1,15 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.txt-principal', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.txt-second', {
+    duration: 1800,
+    distance: '90px'
+})
+revelar.reveal('.img-banner', {
+    duration: 1800,
+    distance: '90px'
+})
